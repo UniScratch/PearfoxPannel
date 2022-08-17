efd7e69959fdedb41a8ed9e8c1e47317a9560ece
HydroGest <2445691453@qq.com>
2022-08-17 17:58:36 +0800

    Fix mistakes

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from flask import Flask,render_template
 from mirai import Mirai, WebSocketAdapter, FriendMessage
-import yaml,json,_thread
+from plugins import *
+import yaml,json
 
-def botrun():
-    bot.run()
 # 导入配置
 with open('./config.yml', 'r', encoding='utf-8') as f:
-    config = yaml.load(stream=f, Loader=yaml.FullLoader)
-
-
+	config = yaml.load(stream=f, Loader=yaml.FullLoader)
+print(config)
 bot = Mirai(
-    qq=config['account'],
+    qq=config.account,
     adapter=WebSocketAdapter(
-        verify_key=config['verifyKey'], host=config['host'], port=config['port']
+        verify_key=config.verifyKey, host=config.host, port=config.port
     )
 )
 
-_thread.start_new_thread(botrun, ())
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello(name=None):
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000, host="0.0.0.0")
